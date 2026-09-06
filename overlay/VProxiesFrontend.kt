2cc9d796e0f88529c63d1774b6d8324d04d9ae24
vproxies20 <app@vproxies.app>
2026-09-07 02:55:16 +0700

    Fix Compose Box scope import

--- overlay/VProxiesFrontend.kt
+++ overlay/VProxiesFrontend.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
